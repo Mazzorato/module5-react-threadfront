b68595a0617fbdbb3d8bb298a602315f0c2eb534
Mazzorato ALEXIS <alexis.mazzorato@gmail.com>
2025-12-09 14:01:25 +0100

    Affichage brute maquette

--- thread-front/src/App.jsx
+++ thread-front/src/App.jsx
@@ -1,20 +1,27 @@
-import { useState } from 'react'
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import { useState } from 'react'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Feed } from "./components/Feed/Feed.jsx";
+import { Post } from "./components/Post/Post.jsx";
+import { NewPost } from "./components/NewPost/NewPost.jsx";
+import { NewComment } from "./components/Comment/NewComment.jsx";
 
-import HomePage from './components/HomePage.jsx'
+import HomePage from "./components/HomePage.jsx";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/Feed" element={<Feed />} />
+          <Route path="/Post" element={<Post />} />
+          <Route path="/NewPost" element={<NewPost />} />
+          <Route path="/NewComment" element={<NewComment />} />
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
