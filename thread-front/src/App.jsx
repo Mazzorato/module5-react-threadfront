c7403dff4c6f668bfe38aff4fb0bf27a188373aa
Roxane Lelievre <x-roxane.lelievre@hotmail.fr>
2025-12-09 11:41:28 +0100

    lien cliquable centré

--- thread-front/src/App.jsx
+++ thread-front/src/App.jsx
@@ -1,22 +1,23 @@
-import { useState } from 'react'
-import './App.css'
-import { Register } from './components/auth/register.jsx'
-import { Login } from './components/auth/login.jsx'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useState } from "react";
+import "./App.css";
+import { Register } from "./components/auth/register.jsx";
+import { Login } from "./components/auth/login.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import HomePage from './components/HomePage.jsx'
+import HomePage from "./components/HomePage.jsx";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
