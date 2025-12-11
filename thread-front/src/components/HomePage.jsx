e47f86e93a6549e9ad9426b8112151a263f52837
Javier <Javier@gmail.com>
2025-12-11 15:17:49 +0100

    ajout de component commentcard et css

--- thread-front/src/components/HomePage.jsx
+++ thread-front/src/components/HomePage.jsx
@@ -1,14 +1,14 @@
 import Title from "./shared/Title";
 import PostCard from "./shared/PostCard";
 import CommentCard from "./shared/CommentCard";
-import Footer from "./shared/NavBar";
+import NavBar from "./shared/NavBar";
 
 
 export default function HomePage(){
 
     return (
         <div>
-            <Footer />
+            
         </div>
     )
 }
