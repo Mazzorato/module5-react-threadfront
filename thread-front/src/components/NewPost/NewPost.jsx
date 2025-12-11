5f0b655d0792caf4bab9da3068455d75a4d73a06
Mazzorato ALEXIS <alexis.mazzorato@gmail.com>
2025-12-11 16:32:17 +0100

    Ajout newpost

--- thread-front/src/components/NewPost/NewPost.jsx
+++ thread-front/src/components/NewPost/NewPost.jsx
@@ -1,24 +1,23 @@
-
 import "./NewPost.css";
 
 export function NewPost() {
-
-    return (
-
-
-   <div>
-        <form method="post">
-      <input type="textarea" name="post" placeholder="Tapez votre texte ici" required />
-   <button type="submit">Poster ! </button>
-    </form>
-        
-        
-        
-
-
-        
-            
-            
+  return (
+    <div>
+      <h1> New Post</h1>
+      <form method="post">
+        <div className="post-preview">
+          <input
+            type="textarea"
+            name="post"
+            placeholder="Tapez votre post ici ..."
+            required
+          />
+          <p>15:25 13 août 25</p>
         </div>
-    )
+        <button type="submit">
+          Poster !{" "}
+        </button>
+      </form>
+    </div>
+  );
 }
