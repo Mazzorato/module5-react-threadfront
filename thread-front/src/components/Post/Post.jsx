b8db61fb27ca56669462f4f01e6cc27222ed56bc
Javier <Javier@gmail.com>
2025-12-12 12:09:08 +0100

    ajout de postcar, commentcard, title et navbar dans la route post

--- thread-front/src/components/Post/Post.jsx
+++ thread-front/src/components/Post/Post.jsx
@@ -1,14 +1,35 @@
 
 import "./Post.css";
+import Title from "../shared/Title.jsx";
+import PostCard from "../shared/PostCard.jsx";
+import CommentCard from "../shared/CommentCard.jsx";
+import NavBar from "../shared/NavBar.jsx";
+
 
 export function Post() {
 
     return( 
 
         <div>
-            16
+            <Title title={"Post"} />
+            <PostCard author={"@Ryu-du57"} 
+            content={"Aujourd'hui je me suis promene sous la pluie."}
+            date={"11:50 12 dec 25"}/>
+            <CommentCard author={"@Sakura-chan"} 
+            content={"Moi aussi j'aime la pluie !"}
+            date={"12:00 12 dec 25"}/>
+            <CommentCard author={"@Kenjiro"} 
+            content={"La pluie c'est la vie."}
+            date={"12:05 12 dec 25"}/>
+            <CommentCard author={"@Sakura-chan"} 
+            content={"Moi aussi j'aime la pluie !"}
+            date={"12:00 12 dec 25"}/>
+            <CommentCard author={"@Kenjiro"} 
+            content={"La pluie c'est la vie."}
+            date={"12:05 12 dec 25"}/>
 
-            boucle
+            <NavBar />
+            
         </div>
     )
 }
--- thread-front/src/components/shared/NavBar.jsx
+++ thread-front/src/components/shared/NavBar.jsx
@@ -2,13 +2,13 @@ import "./NavBar.css"
 export default function navBar() {
     return (
         <footer className="navBar">
-            <button className="navBar-button">
+            <button className="navBar-button-plus">
                 <img src="../src/assets/Logo/circle-plus-solid-full.svg" />
             </button>
-            <button className="navBar-button">
+            <button className="navBar-button-profile">
                 <img src="../src/assets/Logo/circle-user-solid-full.svg" />
             </button>
-            <button className="navBar-button">
+            <button className="navBar-button-message">
                 <img src="../src/assets/Logo/message-solid-full.svg" />
             </button>
 
