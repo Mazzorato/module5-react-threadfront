ccf4c3a4fdbe00677f632f24b9a50a4ec434699c
Mazzorato ALEXIS <alexis.mazzorato@gmail.com>
2025-12-15 16:56:40 +0100

    Ajout fetch register

--- thread-front/src/components/auth/register.jsx
+++ thread-front/src/components/auth/register.jsx
@@ -1,19 +1,54 @@
 // import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./register.css";
 import Title from "../shared/Title.jsx";
 
 export function Register() {
-  return (
-    <div className="registerPage">
+  const navigate = useNavigate();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const pseudo = formData.get("pseudo");
+    const email = formData.get("email");
+    const password = formData.get("password");
+    const confirmPassword = formData.get("confirm_password");
+
+    try {
+      const response = await fetch("http://localhost:3000/api/users/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: pseudo,
+          email: email,
+          password: password,
+          confirmPassword: confirmPassword,
+        }),
+      });
+      if (response.ok) {
+        console.log("Inscription réussie");
+        navigate("/login"); // Redirection vers la page de login
+      } else {
+        console.error("Erreur serveur lors de l'inscription");
+      }  
+      } catch (err) {
+        console.error(err);
+      }
+    } 
+    return (
+      <div className="registerPage">
       <Title title={"Création de compte"} />
-      <form className="registerForm" method="post">
+      <form className="registerForm" onSubmit={handleSubmit}>
         <input
           className="pseudoR zoneT"
           type="text"
           name="pseudo"
           placeholder="@Pseudo"
           required
-        />
+          />
 
         <input
           className="emailR zoneT"
@@ -45,4 +80,6 @@ export function Register() {
       </form>
     </div>
   );
+
 }
+
