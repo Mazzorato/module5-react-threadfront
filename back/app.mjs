003f952fbabe41cfe81bce7427d6a99578868ff7
Roxane Lelievre <x-roxane.lelievre@hotmail.fr>
2026-05-28 10:30:22 +0200

    Revert "Add Dockerfiles, compose.yaml, .dockerignore and update README"
    This reverts commit 4b7a48a449a995e19a9c93d6cedb0f5adbff16a3.

--- back/app.mjs
+++ back/app.mjs
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:8089"],
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization']
--- back/config/config.mjs
+++ back/config/config.mjs
@@ -1,6 +1,6 @@
 export const db = 
 {
-    host: 'bdd',
+    host: '127.0.0.1',
     user: 'root',
     password: 'root',
     database: 'Thread_database',
